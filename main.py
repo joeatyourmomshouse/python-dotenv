3e5aa41994884b31f3937876d469b7b6e15ae876
Virat <viratmpriya@yahoo.com>
2026-03-31 20:10:39 +0000

    hmm

--- main.py
+++ main.py
@@ -5,18 +5,5 @@ import requests
 load_dotenv()
 key = os.getenv("key")
 
-city = "Torronto"
-
-url = (f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=imperial")
-
-response = requests.get(url)
-data = response.json()
-
-if response.status_code == 200:
-    temp = data["main"]["temp"]
-    print(f"The current temperature in {city} is {temp}°F.")
-
-else:
-    print(f"Error fetching weather data: {data['message']}")
 
 
